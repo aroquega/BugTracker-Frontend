4dfaee2f0e5a76c0f831e53f670dd25f82b415bb
Hristo Roque <aroquega@unsa.edu.pe>
2021-04-06 18:07:18 -0500

    Start Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from "./Pages/HomePage";
+function App() {
+  return (
+      <main>
+          <HomePage/>
+      </main>
+  );
+}
+
+export default App;
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage(){
+    return (
+        <main>
+            <h1>BugTracker</h1>
+            {/*TODO: Render projects from API*/}
+        </main>
+    )
+}
+
+export default HomePage;
